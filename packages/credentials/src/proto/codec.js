71c3f7c21e4ea1295de1658864beb293304d18d9
Dmytro Maretskyi <maretskii@gmail.com>
2020-07-28 20:56:14 +0300

    Generate TS definitions for protobufs

--- packages/credentials/src/proto/codec.js
+++ packages/credentials/src/proto/codec.js
@@ -4,19 +4,11 @@
 
 import { Codec } from '@dxos/codec-protobuf';
 
-import AuthDefs from './gen/auth.json';
-import GreetDefs from './gen/greet.json';
-import IdentityDefs from './gen/identity.json';
-import PartyDefs from './gen/party.json';
-import SignedDefs from './gen/signed.json';
+import Defs from './gen/bundle.json';
 
 // TODO(burdon): Common system-wide codec.
 export const codec = new Codec('dxos.credentials.Message')
-  .addJson(AuthDefs)
-  .addJson(GreetDefs)
-  .addJson(IdentityDefs)
-  .addJson(PartyDefs)
-  .addJson(SignedDefs)
+  .addJson(Defs)
   .build();
 
 // TODO(dboreham): what is this validating and why would the caller be calling it?
