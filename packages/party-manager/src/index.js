2477667fed0f56dfac446f42b130ba9dbd8c4eab
Thomas E Lackey <telackey@bozemanpass.com>
2020-10-13 12:34:22 -0500

    Switch version.

--- packages/party-manager/src/index.js
+++
@@ -1,8 +0,0 @@
-//
-// Copyright 2020 DXOS.org
-//
-
-export * from './invitation-descriptor';
-export * from './invite-details';
-export { PartyManager } from './party-manager';
-export { waitForCondition } from './util';
