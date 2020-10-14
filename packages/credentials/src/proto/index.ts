5759c3e768be71c8a79fb365b2187f6d5c52f0b1
Dmytro Maretskyi <maretskii@gmail.com>
2020-10-14 13:41:51 +0200

    Export protobuf types

--- packages/credentials/src/proto/index.ts
+++ packages/credentials/src/proto/index.ts
@@ -3,3 +3,8 @@
 //
 
 export * from './codec';
+export * from './gen/dxos/credentials'
+export * from './gen/dxos/credentials/auth'
+export * from './gen/dxos/credentials/greet'
+export * from './gen/dxos/credentials/identity'
+export * from './gen/dxos/credentials/party'
